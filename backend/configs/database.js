e3ea128cdeb8a9ca0bd93c2b37bee31ecdda546b
Micooo24 <mico.rabino24@gmail.com>
2025-10-20 19:48:34 +0800

    backend changes

--- backend/configs/database.js
+++ backend/configs/database.js
@@ -1,7 +1,7 @@
 const mongoose =require("mongoose")
 
 const connectToDatabase = () =>{
-    mongoose.connect(process.env.DB_URI).then(con=>{
+    mongoose.connect(process.env.MONGO_URI).then(con=>{
         console.log(`connected to mongoDB with host ${con.connection.host}`)
     })
 }
